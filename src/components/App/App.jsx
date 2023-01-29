a4864ed1a1c7447868078d8498f0c2abf465e2f2
ViktorSolomianiy <xmegasystem35@gmail.com>
2023-01-29 17:33:15 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,7 +19,6 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
-        alignItems: 'center',
         fontSize: 40,
         color: '#010101',
       }}
